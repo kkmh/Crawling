24a7fd4f8dc4b56aa2be55ed1fd051966f048bca
wind1237 <wind1237@gmail.com>
2019-01-23 10:31:27 +0900

    Create sony_image.py

---
+++ sony_image.py
@@ -0,0 +1,18 @@
+```
+사이트 이미지 다운 :에러 버전 
+```
+
+
+import urllib.request
+import os
+
+
+os.chdir("/Users/minhyeongguk/Downloads") #your path
+i=1;
+
+while i<200:
+    try:
+        urllib.request.urlretrieve("http://www.nexusbook.com/studymail/image/"+ str(i)+"ho.jpg",str(i)+".jpg")
+    except:
+        print("not possible" + str(i))
+    i+=1;
