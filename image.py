8a04038b97d08975dac2a2e0cbf9d70fbeb433c4
wind1237 <wind1237@gmail.com>
2019-01-23 10:29:16 +0900

    1.0

---
+++ image.py
@@ -0,0 +1,16 @@
+
+
+
+import urllib.request
+import os
+import requests
+
+
+os.chdir("/Users/minhyeongguk/Downloads/") #your path
+i=1
+
+while i < 199:
+    urllib.request.urlretrieve("http://www.nexusbook.com/studymail/image/"+ str(i) + "ho.jpg", str(i)+".jpg")
+    i+=1
+
+
